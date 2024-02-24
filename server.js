a7b52da43b00b831437d836bd8e025b86eaaf0a3
rishiekugathasan <rishie2001@gmail.com>
2024-02-24 17:39:29 -0500

    finished up the home page for the time being. Need to work on Projects section and then Links should be easy.

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const path = require('path');
+const bodyparser = require('body-parser'); 
+
 const app = express();
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000: http://localhost:3000");
-});
+app.use(bodyparser.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static('views'));
+
+app.set('views', path.join(__dirname, 'views'));
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
+});
+
+app.get('/main', (req, res) => {
+    res.sendFile(__dirname + '/views/main.html');
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000: http://localhost:3000");
 });
